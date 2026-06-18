2bf97fd82b60bbef9aefb6fd3bf45000e8737e20
Aman Abdullayev <aman.abdullayev@zalando.de>
2026-06-18 20:14:34 +0200

    remove polyfill

--- .github/scripts/generate-blog-pages.js
+++ .github/scripts/generate-blog-pages.js
@@ -560,7 +560,6 @@ function createBlogPostTemplate(post, renderedContent) {
             }
         };
     </script>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 </head>
 <body>
